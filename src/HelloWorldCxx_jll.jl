ec363e0c17148d630434ddaaaa22d5ace46c28b4
root <root@amdci7.julia.csail.mit.edu>
2026-05-01 07:03:07 +0000

    HelloWorldCxx v1.1.2

--- src/HelloWorldCxx_jll.jl
+++ src/HelloWorldCxx_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule HelloWorldCxx_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module HelloWorldCxx_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module HelloWorldCxx_jll
 
-JLLWrappers.@generate_main_file_header("HelloWorldCxx")
-JLLWrappers.@generate_main_file("HelloWorldCxx", UUID("ae6da99f-25d9-581d-be6a-a93134b8bbac"))
-end  # module HelloWorldCxx_jll
